f442d2ea71c5de8438234f012bfab64432254e5b
MatiasMartinez <matiasnicomartinez09@gmail.com>
2020-09-10 20:15:50 -0300

    Update2

--- SoftwareAdministrativo/Software Administrativo/My Project/Application.Designer.vb
+++ SoftwareAdministrativo/Software Administrativo/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Software_Administrativo.Agregar_Materiales
+            Me.MainForm = Global.Software_Administrativo.Form1
         End Sub
     End Class
 End Namespace
